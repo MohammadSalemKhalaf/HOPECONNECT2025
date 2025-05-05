e47adbe65b4b9811191378937cfcd84a5f4b467c
Mohammad Murad Esawi <146820485+mohammadEsawi@users.noreply.github.com>
2025-05-06 01:02:40 +0300

    commit message for server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import users from "./routes/userRoutes.js";
 import donation from "./routes/donationRoutes.js";
 import volunteers from "./routes/volunteerRoutes.js";
 import emergencyCampaigns from "./routes/emergencyCampaignRoutes.js";
+import orphanageRoutes from "./routes/orphanageRoutes.js";
 const app = express();
 
 const PORT = process.env.PORT || 8000;
@@ -20,13 +21,14 @@ app.use("/api/orphans", orphans);
 
 app.use("/api/users", users);
 
-app.use("/api/donations",donation);
+app.use("/api/donations", donation);
 
-app.use("/api/volunteerstask",volunteers)
+app.use("/api/volunteerstask", volunteers);
 
+app.use("/api/orphanage", orphanageRoutes);
 
 //feature 5
-app.use('/api/emergency-campaigns',emergencyCampaigns) // create campaign
+app.use("/api/emergency-campaigns", emergencyCampaigns); // create campaign
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
