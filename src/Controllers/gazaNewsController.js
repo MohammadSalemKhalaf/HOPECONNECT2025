84a12af4215b155b3187b68a257c6f8bab69f69d
JawadHamdan03 <eng.jawad121@hotmail.com>
2025-05-17 01:22:42 +0300

    remove dublicate

--- src/Controllers/gazaNewsController.js
+++ src/Controllers/gazaNewsController.js
@@ -3,9 +3,15 @@ import axios from   "axios";
 export const getArticles = async (req, res) => {
 try{
 const news = await axios.get(`https://newsapi.org/v2/everything?q=Gaza&apiKey=${process.env.NEWS_API_KEY}`); 
-const articles = news.data.articles;
-return res.status(200).json({articles})
+if(req.params.number){
+  const articles = news.data.articles.slice(0,req.params.number);
+  return res.status(200).json({articles})
 }
+else
+{
+  const articles = news.data.articles;
+  return res.status(200).json({articles})
+}}
 catch (error) {
     res.status(500).json({ message: error.message });
   }
