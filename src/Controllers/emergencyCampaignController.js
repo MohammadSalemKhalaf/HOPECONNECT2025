ad9b89859fc6fddc82ea57592d5ba84874d7aaf8
JawadHamdan03 <eng.jawad121@hotmail.com>
2025-05-19 12:31:57 +0300

    remove unnecessery files and folders

--- src/Controllers/emergencyCampaignController.js
+++ src/Controllers/emergencyCampaignController.js
@@ -1,10 +1,9 @@
 import pool from "../config/database.js";
-import { sendEmail } from "../utils/emailService.js";
+
 
 export const createCampaign = async (req, res) => {
     const { title, description, goal_amount, start_date, end_date } = req.body;
-    const sql = `INSERT INTO emergency_campaign (title, description, goal_amount, start_date, end_date)
-                 VALUES (?, ?, ?, ?, ?)`;
+    const sql = `insert into emergency_campaign (title, description, goal_amount, start_date, end_date)  values (?, ?, ?, ?, ?)`;
     await pool.query(sql, [title, description, goal_amount, start_date, end_date]);
     res.status(201).json({ message: 'Campaign created successfully' });
   }
@@ -14,31 +13,21 @@ export const createCampaign = async (req, res) => {
     const { user_id, amount } = req.body;
   
     try {
-      // Insert donation
       await pool.query(
-        `INSERT INTO emergency_donation (campaign_id, user_id, amount) VALUES (?, ?, ?)`,
+        `insert into emergency_donation (campaign_id, user_id, amount) values (?, ?, ?)`,
         [campaignId, user_id, amount]
       );
   
-      // Update collected amount
+      
       await pool.query(
-        `UPDATE emergency_campaign SET collected_amount = collected_amount + ? WHERE id = ?`,
+        `UPDATE emergency_campaign set collected_amount = collected_amount + ? where id = ?`,
         [amount, campaignId]
       );
   
-      // Fetch user and campaign info
-      const [[user]] = await pool.query(`SELECT full_name, email FROM user WHERE id = ?`, [user_id]);
-      const [[campaign]] = await pool.query(`SELECT title FROM emergency_campaign WHERE id = ?`, [campaignId]);
   
-      // Send email
-      const subject = `📢 شكراً لمساهمتك في حملة: ${campaign.title}`;
-      const text = `مرحباً ${user.full_name}،\n\nشكراً لتبرعك بمبلغ ${amount} دولار لحملة "${campaign.title}".\n\nنحن نقدّر دعمك.\n\nفريق HopeConnect.`;
-  
-      await sendEmail(user.email, subject, text);
-  
-      res.status(200).json({ message: "Donation received and email sent." });
+      res.status(200).json({ message: "Donation received" });
     } catch (error) {
-      console.error("Donation or email failed:", error);
+      console.error("Donation  failed:", error);
       res.status(500).json({ message: "Internal Server Error" });
     }
   }
@@ -47,7 +36,7 @@ export const createCampaign = async (req, res) => {
 
   export const getAllCampaigns = async (req, res) => {
     try {
-      const [campaigns] = await pool.query("SELECT * FROM emergency_campaign");
+      const [campaigns] = await pool.query("select * from emergency_campaign");
       res.status(200).json(campaigns);
     } catch (error) {
       console.error(error);
